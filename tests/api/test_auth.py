a8a7c908594af3c1ea5272388805ec9c9ee60329
tcarreira <tiago.g.carreira+git@gmail.com>
2020-04-25 11:38:08 -0300

    test logout

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -5,6 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.test import APIRequestFactory
 
 from api.models import User
+from api.views import Logout
 
 
 class TestAuthentication(TestCase):
@@ -14,13 +15,28 @@ class TestAuthentication(TestCase):
             user.set_password(password)
             user.save()
 
-    def test_login(self):
+    def test_login_and_logout(self):
         factory = APIRequestFactory()
         request = factory.post(
             "/api/v1/login", {"username": "admin", "password": "adminpass"}, format="json"
         )
         response = obtain_auth_token(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("token", response.data)
+
+        # Logout this token
+        token = response.data["token"]
+        request = factory.get("/api/v1/logout", HTTP_AUTHORIZATION="Token {}".format(token))
+        view = Logout.as_view()
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNone(response.data)
+
+        # Retry same token and fails as unauthorized
+        view = Logout.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_login_wrong_credentials(self):
         factory = APIRequestFactory()
