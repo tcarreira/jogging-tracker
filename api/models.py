107a6e113c4dc0f86df80c472e7f5b7ff690e0ae
tcarreira <tiago.g.carreira+git@gmail.com>
2020-04-27 21:47:35 -0300

    fix nulls mess

--- api/models.py
+++ api/models.py
@@ -16,12 +16,12 @@ class Activity(models.Model):
             ("crud_others_activities", "Can change any activity (owned by other user)"),
         ]
 
-    date = models.DateTimeField(null=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
-    distance = models.PositiveIntegerField(null=True)
-    weather = models.ForeignKey("Weather", on_delete=models.DO_NOTHING)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    date = models.DateTimeField(null=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    distance = models.PositiveIntegerField(null=False)
+    weather = models.ForeignKey("Weather", null=True, on_delete=models.DO_NOTHING)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
 
     def __str__(self):
         return "{}: {} - {}".format(self.user, self.date, self.distance)
@@ -29,8 +29,8 @@ class Activity(models.Model):
 
 class Weather(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=80, null=True)
-    description = models.CharField(max_length=254, null=True)
+    title = models.CharField(max_length=80, null=False)
+    description = models.CharField(max_length=254, null=False)
 
     def __str__(self):
         return self.title
