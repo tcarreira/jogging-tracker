9e9d4b11ae8636c0d158b0bd710b6d2d804a11ec
tcarreira <tiago.g.carreira+git@gmail.com>
2020-04-28 23:19:34 -0300

    save weather on create activity

--- api/models.py
+++ api/models.py
@@ -1,6 +1,10 @@
+from typing import Optional
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from .external_sources import WeatherProvider
 
 
 class User(AbstractUser):
@@ -26,6 +30,24 @@ class Activity(models.Model):
     def __str__(self):
         return "{}: {} - {}".format(self.user, self.date, self.distance)
 
+    def save(self, *args, **kwargs):
+        if self.pk is None and hasattr(self, "latitude") and hasattr(self, "longitude"):
+            # on create, get weather
+            weather_dict = WeatherProvider().getWeather(
+                float(self.latitude), float(self.longitude)
+            )
+            weather = Weather.get_or_create(**weather_dict)
+            self.weather = weather
+
+        elif "form" in kwargs and (
+            "latitude" in kwargs["form"].changed_data
+            or "longitude" in kwargs["form"].changed_data
+        ):
+            # if updating lat/lon, the weather info is not valid anymore
+            self.weather = None
+
+        super(Activity, self).save(*args, **kwargs)
+
 
 class Weather(models.Model):
     id = models.AutoField(primary_key=True)
@@ -34,3 +56,19 @@ class Weather(models.Model):
 
     def __str__(self):
         return self.title
+
+    @staticmethod
+    def get_or_create(
+        id: Optional[int] = None,
+        title: Optional[str] = None,
+        description: Optional[str] = None,
+    ) -> Optional["Weather"]:
+        """Create Weather Object is not exists (by id). 
+        Returns None if incomplete or no data is provided"""
+        if id is None or title is None or description is None:
+            return None
+
+        weather, _ = Weather.objects.get_or_create(
+            id=id, defaults={"title": title, "description": description}
+        )
+        return weather
--- tests/api/test_activities.py
+++ tests/api/test_activities.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from django.test import TestCase
 from django.utils import timezone
 from rest_framework import status
@@ -6,27 +8,29 @@ from api.models import Activity, User, Weather
 
 
 class TestActivities(TestCase):
-    def setUp(self):
+    @mock.patch("api.external_sources.WeatherProvider.getWeather")
+    def setUp(self, mock_get_weather):
+        mock_get_weather.return_value = {
+            "id": 1,
+            "title": "Clouds",
+            "description": "broken clouds",
+        }
+
         users = [
             User.objects.create_user(username="user1", password="123456"),
             User.objects.create_user(username="user2", password="123456"),
             User.objects.create_superuser(username="useradmin", password="123456"),
         ]
 
-        weather = Weather.objects.create(title="abc", description="abc")
-
         activity_common = {
-            "distance": 1,
-            "latitude": 1,
-            "longitude": 1,
-            "weather": weather,
+            "distance": 10,
+            "latitude": 20,
+            "longitude": 30,
         }
 
         self.this_now = timezone.now()
 
-        Activity.objects.create(
-            date=self.this_now, user=users[0], **activity_common,
-        )
+        a = Activity.objects.create(date=self.this_now, user=users[0], **activity_common,)
         Activity.objects.create(
             date=self.this_now, user=users[0], **activity_common,
         )
@@ -57,6 +61,10 @@ class TestActivities(TestCase):
         response = self.client.get("/api/v1/activities/1",)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["distance"], 10)
+        self.assertEqual(response.data["latitude"], 20.0)
+        self.assertEqual(response.data["longitude"], 30.0)
+        self.assertEqual(response.data["weather"], "Clouds")
 
     def test_access_to_others_activity_not_found(self):
         self.assertTrue(self.client.login(username="user2", password="123456"))
